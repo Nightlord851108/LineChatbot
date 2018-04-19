d666d5938d5f80a9dc5502c9d1829a34daee88e2
Nightlord851108 <nightlord851108@gmail.com>
2018-04-19 19:28:51 +0800

    Try to work on Heroku

--- src/app.py
+++ src/app.py
@@ -13,9 +13,9 @@ from linebot.models import (
 app = Flask(__name__)
 
 # Channel Access Token
-line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+line_bot_api = LineBotApi('ZtmdrSOL3jYcjtNFpoAWNnmFbRivHNUR8U+w3ftRnNF0ODl5G5w+aT3X1LEm6616qnHvJlxQBr3N9YhXRp1XyqSlWWOQRb1W9lx0UPX2FMPbVNNCIhIBwsXKRt0lvz+1hOa+Qe+IhMX7hfcenFWIJwdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
-handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+handler = WebhookHandler('62eb9886562f78d9402b8ba72a4f57f4')
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -25,6 +25,7 @@ def callback():
 
     # get request body as text
     body = request.get_data(as_text=True)
+    print(body)
     app.logger.info("Request body: " + body)
 
     # handle webhook body
@@ -38,7 +39,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    message = TextSendMessage(text="Hello, world!")
     line_bot_api.reply_message(
         event.reply_token,
         message)
