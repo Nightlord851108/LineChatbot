b791ed60af6bed400c852dfe790b9cb58256248b
Nightlord851108 <nightlord851108@gmail.com>
2018-04-24 23:36:33 +0800

    Fixed message handler

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def callback():
 def handle_message(event):
     print("*****Input Message In handle_message*****")
     print(event)
-    inputMessage = event['message']['text']
+    inputMessage = event.message.text
     ms = Message(inputMessage)
     replyMessage = ms.getOutput()
     message = TextSendMessage(text=rerply)
